bb68cf7148cf462b73bf480ba1bad4edd52405f9
Steven Van Ingelgem <steven@vaningelgem.be>
2023-03-19 15:20:19 +0100

    Unlink before renaming

--- location_ipfire_db_reader/decompress_db.py
+++ location_ipfire_db_reader/decompress_db.py
@@ -5,8 +5,6 @@ from pathlib import Path
 
 def decompress_xz_file(file_path: str | os.PathLike):
     file_path = Path(file_path)
-
-    assert file_path.suffix.lower() == ".xz"
     target = file_path.with_suffix(".tmp")
 
     with lzma.open(file_path, "rb") as f_in:
@@ -15,4 +13,5 @@ def decompress_xz_file(file_path: str | os.PathLike):
             for chunk in iter(lambda: f_in.read(1024 * 1024), b""):
                 f_out.write(chunk)
 
+    file_path.unlink()
     target.rename(file_path)
